0eefaac48b8ce9f86f5045586e781ffd339d69a0
VuCuongHD96 <xuancuonghd1102@gmail.com>
2023-04-10 22:42:18 +0700

    test LINE

--- ImageTransition/ViewController.swift
+++ ImageTransition/ViewController.swift
@@ -10,13 +10,39 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let url = URL(string: "https://line.me/R/")!
 
+    
     lazy var imageTransitionManager = ImageTransitionManager(imageView: myImageView)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupMyImageView()
+        
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if UIApplication.shared.canOpenURL(url) {
+            titleLabel.text = "My App is installed"
+        } else {
+            titleLabel.text = "My App is not installed"
+        }
+    }
+    
+    @IBAction func deepLinkAction(_ sender: Any) {
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func sendMessAction(_ sender: Any) {
+        let url2 = URL(string: "https://line.me/R/share?text=abc")!
+        UIApplication.shared.open(url2)
+
     }
     
     private func setupMyImageView() {
@@ -29,7 +55,7 @@ class ViewController: UIViewController {
         
         
         let imageScreen = ImageViewController()
-//        imageScreen.transitioningDelegate = self
+        //        imageScreen.transitioningDelegate = self
         imageScreen.transitionImageView = myImageView
         imageScreen.modalPresentationStyle = .fullScreen//.custom
         present(imageScreen, animated: false)
@@ -37,14 +63,14 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UIViewControllerTransitioningDelegate {
-
+    
     func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-//        myImageView.isHidden = true
+        //        myImageView.isHidden = true
         return imageTransitionManager.presentTransition
     }
-
+    
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-//        myImageView.isHidden = false
+        //        myImageView.isHidden = false
         return imageTransitionManager.dismissTransition
     }
 }
